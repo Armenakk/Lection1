7b5142baed5b61f8a76387db5e0c80e3a1dd40fe
«Armenak <armenakk@yandex.ru>
2023-09-16 15:58:54 +0300

    Example006

---
+++ Example006_New/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Ведите имя ползователя ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "маша")
+{
+    Console.Write("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(userName);
+}
